63ef1ba5a03945e6f6114514cb440af9267e4b5c
khristovv <kamen.hristov.98@gmail.com>
2023-06-27 22:54:49 +0300

    update `classify` method of `AbstractTree` class

--- grove/trees/base_tree.py
+++ grove/trees/base_tree.py
@@ -7,15 +7,15 @@ from grove.nodes import Node
 
 class AbstractTree:
     def train(self, x: pd.DataFrame, y: pd.DataFrame):
-        """A method that builds the decision tree from the training set (x, y)."""
+        """A method that trains a decision tree model from the training set (x, y)."""
         raise NotImplementedError
 
     def test(self, x: pd.DataFrame, y: pd.DataFrame):
-        """A method that tests the decision tree on the test set (x, y)."""
+        """A method that tests the decision tree model on the test set (x, y)."""
         raise NotImplementedError
 
-    def classify(self):
-        """A method"""
+    def classify(self, data: pd.DataFrame, y_label: str):
+        """A method that classifies new data using the trained decision tree model."""
         raise NotImplementedError
 
 
