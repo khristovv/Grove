145a02928528bb8ea5f7c2a6ecea3b01de219f7a
khristovv <kamen.hristov.98@gmail.com>
2023-07-03 23:50:34 +0300

    add an `Intermediate` exmaple dataset

--- classification_example.py
+++ classification_example.py
@@ -1,23 +1,50 @@
 # all training data is used from https://www.kaggle.com/datasets/yersever/500-person-gender-height-weight-bodymassindex
+import argparse
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from grove.trees import ClassificationTree
 
-X_PATH = "./data/Classification/data_x.csv"
-Y_PATH = "./data/Classification/data_y.csv"
-CONFIG_PATH = "./data/Classification/config.csv"
 
-if __name__ == "__main__":
+def load_simple_dataset() -> tuple[pd.DataFrame, pd.Series, pd.DataFrame, str]:
+    # DATA_PATH = "./data/Classification/Simple/data.csv"
+    X_PATH = "./data/Classification/Simple/data_x.csv"
+    Y_PATH = "./data/Classification/Simple/data_y.csv"
+    CONFIG_PATH = "./data/Classification/Simple/config.csv"
+
     x = pd.read_csv(X_PATH)
     y = pd.read_csv(Y_PATH)
     config = pd.read_csv(CONFIG_PATH)
 
+    # y = x["Index"]
+    # x.drop("Index", axis=1, inplace=True)
+
+    y_dtype = "bin"
+
+    return x, y, config, y_dtype
+
+
+def load_intermediate_dataset() -> tuple[pd.DataFrame, pd.Series, pd.DataFrame, str]:
+    DATA_PATH = "./data/Classification/Intermediate/data.csv"
+    CONFIG_PATH = "./data/Classification/Intermediate/config.csv"
+
+    x = pd.read_csv(DATA_PATH, index_col="UDI")
+    y = x["Machine failure"]
+    x.drop("Machine failure", axis=1, inplace=True)
+    config = pd.read_csv(CONFIG_PATH)
+
+    y_dtype = "bin"
+
+    return x, y, config, y_dtype
+
+
+def main(x: pd.DataFrame, y: pd.Series, config: pd.DataFrame, y_dtype: str) -> None:
     x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1)
 
     tree_model = ClassificationTree(
         encoding_config=config,
-        y_dtype="bin",
+        y_dtype=y_dtype,
         max_children=3,
         min_samples_per_node=50,
         max_depth=5,
@@ -46,7 +73,34 @@ if __name__ == "__main__":
         x=x_test,
         y=pd.DataFrame(y_test),
         save_results=True,
-        output_dir="test_results_classification"
+        output_dir="test_results_classification",
     )
     print(test_results)
     print("============================== Done ==============================\n")
+
+
+SIMPLE = "s"
+INTERMEDIATE = "i"
+
+OPTIONS = [SIMPLE, INTERMEDIATE]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run classification examples")
+    parser.add_argument(
+        "--dataset",
+        "-d",
+        dest="dataset",
+        default=SIMPLE,
+        help="The dataset to use for the example. Options are: " + ", ".join(OPTIONS),
+    )
+
+    args = parser.parse_args()
+
+    if args.dataset == SIMPLE:
+        x, y, config, y_dtype = load_simple_dataset()
+    elif args.dataset == INTERMEDIATE:
+        x, y, config, y_dtype = load_intermediate_dataset()
+    else:
+        x, y, config, y_dtype = load_simple_dataset()
+
+    main(x=x, y=y, config=config, y_dtype=y_dtype)
