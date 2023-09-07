973de088b8116403bf0ccbf81d6ca40dd0c30240
khristovv <kamen.hristov.98@gmail.com>
2023-09-07 17:02:06 +0300

    fix type signatures of methods in the `TreeInterface`

--- grove/trees/abstract.py
+++ grove/trees/abstract.py
@@ -2,14 +2,14 @@ import pandas as pd
 
 
 class TreeInterface:
-    def train(self, x: pd.DataFrame, y: pd.DataFrame):
+    def train(self, x: pd.DataFrame, y: pd.Series, *args, **kwargs):
         """A method that trains a decision tree model from the training set (x, y)."""
         raise NotImplementedError
 
-    def test(self, x: pd.DataFrame, y: pd.DataFrame):
-        """A method that tests the decision tree model on the test set (x, y)."""
+    def predict(self, x: pd.DataFrame, y_label: str, *args, **kwargs):
+        """A method that classifies new data using the trained decision tree model."""
         raise NotImplementedError
 
-    def predict(self, data: pd.DataFrame, y_label: str):
-        """A method that classifies new data using the trained decision tree model."""
+    def test(self, x_test: pd.DataFrame, y_test: pd.Series, *args, **kwargs):
+        """A method that tests the decision tree model on the test set (x_test, y_test)."""
         raise NotImplementedError
